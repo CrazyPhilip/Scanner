2c35b4cc3336fd7ae5e237976fe91e5bc0beadfc
飞 程 <cf20090901@outlook.com>
2019-06-15 21:27:20 +0800

    1.采用MaterialDesign主题 2.修改布局 3.新增功能需求

---
+++ Scanner/UserControls/RealTimeCollecting.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using JiebaNet.Segmenter;
+using Microsoft.Win32;
+using Scanner.Model;
+using Scanner.Threads;
+using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace Scanner.UserControls
+{
+    /// <summary>
+    /// RealTimeCollecting.xaml 的交互逻辑
+    /// </summary>
+    public partial class RealTimeCollecting : UserControl
+    {
+        public RealTimeCollecting()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ Scanner/UserControls/Single.xaml.cs
@@ -0,0 +1,274 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using JiebaNet.Segmenter;
+using Microsoft.Win32;
+using Scanner.Model;
+using Scanner.Threads;
+using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace Scanner.UserControls
+{
+    /// <summary>
+    /// Single.xaml 的交互逻辑
+    /// </summary>
+    public partial class Single : UserControl
+    {
+        public Single()
+        {
+            InitializeComponent();
+        }
+
+
+        private static Photo photo = new Photo();
+
+        private void ChooseButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openfiledialog = new OpenFileDialog { Filter = "图像文件|*.jpg;*.png;*.jpeg;*.bmp;*.gif|所有文件|*.*" };
+
+            if ((bool)openfiledialog.ShowDialog())
+            {
+                Original.Source = new BitmapImage(new Uri(openfiledialog.FileName));
+                photo.OriginalPhoto = CvInvoke.Imread(openfiledialog.FileName);  //读取图像
+            }
+
+        }
+
+        private void SharpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            Mat sharpened = new Mat();
+            bool s = sharpen(photo.OriginalPhoto, sharpened);
+
+            if (s)
+            {
+                photo.SharpenedPhoto = sharpened;
+                Sharpened.Source = BitmapSourceConvert.ToBitmapSource(sharpened);
+            }
+        }
+
+        //此部分代码功能是锐化图像，本质上是 扫描图像并访问相邻像素
+        //sharpened_pixel=5*current-left-right-up-down
+        byte saturateCast(double inValue)
+        {
+            /*
+            if (inValue < 0)
+                return 0;
+            else if (inValue > 255)
+                return 255;
+            else
+                return (byte)inValue;
+                */
+
+            if (inValue < 100)
+                return 0;
+            else if (inValue >= 100)
+                return 255;
+            else
+                return (byte)inValue;
+        }
+
+        /*
+        bool sharpen(Mat inImg, Mat outImg)
+        {
+            int nchannel = inImg.NumberOfChannels;
+            int nrows = inImg.Rows;
+            int ncols = inImg.Cols;
+
+            if (nchannel == 1)
+            {
+                var tempImg = inImg.ToImage<Gray, byte>();
+                var tempImg2 = tempImg.Clone();
+                for (int i = 1; i < nrows - 1; ++i)
+                {
+                    for (int j = 1; j < ncols - 1; ++j)
+                    {
+                        tempImg2.Data[i, j, 0] = saturateCast(5 * tempImg.Data[i, j, 0] - tempImg.Data[i - 1, j, 0] - tempImg.Data[i, j - 1, 0] -
+                        tempImg2.Data[i + 1, j, 0] - tempImg.Data[i, j + 1, 0]);
+                    }
+                }
+
+                for (int i = 0; i < nrows; ++i)
+                {
+                    tempImg2.Data[i, 0, 0] = 0;
+                    tempImg2.Data[i, ncols - 1, 0] = 0;
+                }
+
+                for (int j = 0; j < ncols; ++j)
+                {
+                    tempImg2.Data[0, j, 0] = 0;
+                    tempImg2.Data[nrows - 1, j, 0] = 0;
+                }
+                tempImg2.Mat.CopyTo(outImg);
+            }
+
+            else if (nchannel == 3)
+            {
+                var tempImg = inImg.ToImage<Bgr, byte>();
+                var tempImg2 = tempImg.Clone();
+                for (int i = 1; i < nrows - 1; ++i)
+                {
+                    for (int j = 1; j < ncols - 1; ++j)
+                    {
+                        for (int channel = 0; channel < 3; channel++)
+                        {
+
+                            tempImg2.Data[i, j, channel] = saturateCast(5 * tempImg.Data[i, j, channel] - tempImg.Data[i - 1, j, channel] - tempImg.Data[i, j - 1, channel] -
+
+                                tempImg.Data[i + 1, j, channel] - tempImg.Data[i, j + 1, channel]);
+                        }
+                    }
+                }
+
+                for (int i = 0; i < nrows; ++i)
+                {
+                    for (int channel = 0; channel < 3; channel++)
+                    {
+                        tempImg2.Data[i, 0, channel] = 0;
+
+                        tempImg2.Data[i, ncols - 1, channel] = 0;
+                    }
+                }
+
+                for (int j = 0; j < ncols; ++j)
+                {
+                    for (int channel = 0; channel < 3; channel++)
+                    {
+
+                        tempImg2.Data[0, j, channel] = 0;
+
+                        tempImg2.Data[nrows - 1, j, channel] = 0;
+                    }
+                }
+
+                tempImg2.Mat.CopyTo(outImg);
+            }
+
+            else
+            {
+                return false;
+            }
+
+            return true;
+        }
+        */
+
+        bool sharpen(Mat inImg, Mat outImg)
+        {
+            int nchannel = inImg.NumberOfChannels;
+            int nrows = inImg.Rows;
+            int ncols = inImg.Cols;
+
+            if (nchannel == 1)
+            {
+                var tempImg = inImg.ToImage<Gray, byte>();
+                var tempImg2 = tempImg.Clone();
+                for (int i = 1; i < nrows - 1; ++i)
+                {
+                    for (int j = 1; j < ncols - 1; ++j)
+                    {
+                        tempImg2.Data[i, j, 0] = saturateCast(tempImg.Data[i, j, 0]);
+                    }
+                }
+
+                for (int i = 0; i < nrows; ++i)
+                {
+                    tempImg2.Data[i, 0, 0] = 0;
+                    tempImg2.Data[i, ncols - 1, 0] = 0;
+                }
+
+                for (int j = 0; j < ncols; ++j)
+                {
+                    tempImg2.Data[0, j, 0] = 0;
+                    tempImg2.Data[nrows - 1, j, 0] = 0;
+                }
+                tempImg2.Mat.CopyTo(outImg);
+
+                tempImg.Dispose();
+                tempImg2.Dispose();
+            }
+
+            else if (nchannel == 3)
+            {
+                var tempImg = inImg.ToImage<Bgr, byte>();
+                var tempImg2 = tempImg.Clone();
+                for (int i = 1; i < nrows - 1; ++i)
+                {
+                    for (int j = 1; j < ncols - 1; ++j)
+                    {
+                        for (int channel = 0; channel < 3; channel++)
+                        {
+
+                            tempImg2.Data[i, j, channel] = saturateCast(tempImg.Data[i, j, channel]);
+                        }
+                    }
+                }
+
+                for (int i = 0; i < nrows; ++i)
+                {
+                    for (int channel = 0; channel < 3; channel++)
+                    {
+                        tempImg2.Data[i, 0, channel] = 0;
+
+                        tempImg2.Data[i, ncols - 1, channel] = 0;
+                    }
+                }
+
+                for (int j = 0; j < ncols; ++j)
+                {
+                    for (int channel = 0; channel < 3; channel++)
+                    {
+
+                        tempImg2.Data[0, j, channel] = 0;
+
+                        tempImg2.Data[nrows - 1, j, channel] = 0;
+                    }
+                }
+
+                tempImg2.Mat.CopyTo(outImg);
+
+                tempImg.Dispose();
+                tempImg2.Dispose();
+            }
+
+            else
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private void RecognizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            //RecoText.Text = Recognize(photo.SharpenedPhoto);
+            string RecoText = "";
+
+            RecognizeThread thread = new RecognizeThread(photo.OriginalPhoto);  //识别原始图片
+            ThreadStart childref = new ThreadStart(thread.Recognize);
+            Thread childThread = new Thread(childref);
+            childThread.Name = "TextRecognition";
+            childThread.Start(); //启动线程
+            childThread.Join();
+
+            RecoText = thread.recoText;
+            var segmenter = new JiebaSegmenter();
+            var segments = segmenter.Cut(RecoText);
+
+            foreach (var word in segments)
+            {
+                Button button = new Button();
+                button.Height = 30;
+                button.Margin = new Thickness(5);
+                //button.Width = 
+
+                button.Content = word;
+
+                stack.Children.Add(button);
+
+            }
+        }
+    }
+}
